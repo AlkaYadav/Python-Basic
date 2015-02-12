106e4423ba2b374e2402d7a3128e4b055f9af8b2
Alka Yadav <alkyadav@cisco.com>
2015-02-11 21:53:30 -0800

    Added methods for updating dictionary

--- dictionary.py
+++ dictionary.py
@@ -32,6 +32,47 @@ def create_dictionary():
     keys='zen alto hyundai lamborgini ferrari bmw mercedes'.split()
     d=dict.fromkeys(keys,"Default")
     print 'Dictionary created from keys',d
+    return d
+
+def update_dictionary(d):
+    '''
+    1.use setdefault
+    '''
+    print 'Original Dictionary:',sorted(d.items())
+    print'Updatind Dictionary with setdeafult:'
+    d.setdefault('new key1','new value1')
+    print 'New Dictionary:',sorted(d.items())
+
+    '''
+    2.Using simple assignment
+    '''
+    print'Updatind Dictionary with simple assignment:'
+    print 'Original Dictionary:',sorted(d.items())
+    d['a']='value'
+    print 'New Dictionary:',sorted(d.items()) 
+
+    '''
+    3.use get method
+    '''
+    print'Updatind Dictionary with get method:'
+    print 'Original Dictionary:',sorted(d.items())
+    print 'Return value of get:',d.get('b',None)
+    print 'New Dictionary(Not updated!!):',sorted(d.items())
+
+    '''
+    3.use update method
+    '''
+    print'Updatind Dictionary with update method:'
+    e={'a':1,'c':4,'y':7}
+    print 'first dictionary:',sorted(d.items())
+    print 'Another dictionary:',sorted(e.items())
+    d.update(e)
+    print 'New Dictionary after calling update:',sorted(d.items())
+
 
 if __name__=='__main__':
-    create_dictionary()
+    print 'Create new dictionary:'
+    d=create_dictionary()
+    print 'Calling update method'
+    print
+    update_dictionary(d)
