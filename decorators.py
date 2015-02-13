91223791d1f7e6454ae3ef1e7e1286caf545c13f
Alka Yadav <alkyadav@cisco.com>
2015-02-12 21:28:51 -0800

    Adding logging function wrapper

--- decorators.py
+++ decorators.py
@@ -9,7 +9,7 @@ def register(func):
     'to register functions'
     'identity function with side effect'
     'as it adds entry for function in registry'
-    'decorators take func as arg and return func
+    'decorators take func as arg and return func'
     registry[func.__name__]=func
     return func
 
@@ -18,9 +18,20 @@ def add_docstring(func):
         func.__doc__='Unknown documentation'
     return func
 
+def add_logging(func):
+	def wrapper(x):
+				print 'Called',func.__name__,'with ',x
+				ans=func(x)
+				print 'The answer is',ans
+				wrapper.__name__=func.__name__
+				return ans
+	return wrapper
+	
+				
 #######List of functions to register and add_doc####
 @register    #like square=register(square)
 @add_docstring
+@add_logging
 def square(x):
     return x ** x
 
